a82ba4ffc06ba416d4f3d2b18d8523b38c5025ab
yvzyildirim <yavuz@binteraktif.com>
2015-12-29 10:20:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-
+<?php include 'header.php'; ?>
+<div class="container-fluid" style="padding-top:125px;">
+	<div class="row">
+		<div  class="col-lg-12" style="height:900px;">
+			<img id="araba" src="image/araba.png" />
+		</div>
+	</div>
+</div>
+<?php include 'footer.php'; ?>
